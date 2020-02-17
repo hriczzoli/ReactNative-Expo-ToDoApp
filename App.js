7faab43d4e93ab43278794280b0bd90b54f25f6e
hriczzoli <hriczzoli@gmail.com>
2020-02-17 09:47:11 +0000

    initial commit

---
+++ App.js
@@ -0,0 +1,74 @@
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, FlatList, Alert, TouchableWithoutFeedback, Keyboard } from 'react-native';
+import Header from './components/Header'
+import TodoItem from './components/TodoItem'
+import AddTodo from './components/AddTodo'
+import Sandbox from './components/Sandbox'
+
+export default function App() {
+  const [todos, setTodos] = useState([
+    { text: 'buy coffee', key: '1' },
+    { text: 'create an app', key: '2' },
+    { text: 'watch Netflix - chill', key: '3' },
+  ])
+
+  const handleDelete = (key) => {
+    setTodos((prevTodos) => {
+      return prevTodos.filter(todo => todo.key != key)
+    })
+  }
+
+  const submitHandler = (text) => {
+    if(text.length > 3){
+      setTodos((prevTodos) => {
+        return [
+          { text: text, key: Math.random().toString },
+          ...prevTodos
+        ]
+      })
+    } else {
+      Alert.alert('OOOPS!', 'Todos must be over 3 characters long', [
+        {text: 'Understood', onPress: () => console.log('alert closed')}
+      ])
+    }
+
+    
+  }
+
+  return (
+    <TouchableWithoutFeedback onPress={() => {
+      Keyboard.dismiss()
+      console.log('dismissed keyboard')
+    }}>
+      <View style={styles.container}>
+        <Header />
+        <View style={styles.content}>
+          <AddTodo submitHandler={submitHandler}/>
+          <View style={styles.list}>
+            <FlatList
+              data={todos}
+              renderItem={({ item }) => (
+                <TodoItem item={item} pressHandler={handleDelete}/>
+              )}
+            />
+          </View>
+        </View>
+      </View>
+    </TouchableWithoutFeedback>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+  content: {
+    padding: 40,
+    flex: 1,
+  },
+  list: {
+    flex: 1,
+    marginTop: 20,
+  }
+});
---
+++ README.md
@@ -0,0 +1,3 @@
+# Exploring ReactNative-Expo
+
+My first Reactnative-Expo project where I was trying explore some of the features of this platform
